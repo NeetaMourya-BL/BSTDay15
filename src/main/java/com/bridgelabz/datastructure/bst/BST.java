0b7c2983e391a7b05ccf9c8b41e6846078ff5f14
unknown <neetamourya24@gmail.com>
2021-12-18 15:42:50 +0530

    [Neeta mourya] add element in bst UC1

--- src/main/java/com/bridgelabz/datastructure/bst/BST.java
+++ src/main/java/com/bridgelabz/datastructure/bst/BST.java
@@ -1,7 +1,99 @@
 package com.bridgelabz.datastructure.bst;
-
-public class BST {
-	public static void main(String[] args) {
-		System.out.println("Binary search tree created");
-	}
+public class BST{ 
+    //node class that defines BST node
+    class Node { 
+        int key; 
+        Node left, right; 
+   
+        public Node(int data){ 
+            key = data; 
+            left = right = null; 
+        } 
+    } 
+    // BST root node 
+    Node root; 
+  
+   // Constructor for BST =>initial empty tree
+    BST(){ 
+        root = null; 
+    } 
+    //delete a node from BST
+   
+    int minValue(Node root)  { 
+        //initially minval = root
+        int minval = root.key; 
+        //find minval
+        while (root.left != null)  { 
+            minval = root.left.key; 
+            root = root.left; 
+        } 
+        return minval; 
+    } 
+   
+    // insert a node in BST 
+    void insert(int key)  { 
+        root = insert_Recursive(root, key); 
+    } 
+   
+    //recursive insert function
+    Node insert_Recursive(Node root, int key) { 
+          //tree is empty
+        if (root == null) { 
+            root = new Node(key); 
+            return root; 
+        } 
+        //traverse the tree
+        if (key < root.key)     //insert in the left subtree
+            root.left = insert_Recursive(root.left, key); 
+        else if (key > root.key)    //insert in the right subtree
+            root.right = insert_Recursive(root.right, key); 
+          // return pointer
+        return root; 
+    } 
+ 
+// method for inorder traversal of BST 
+    void inorder() { 
+        inorder_Recursive(root); 
+    } 
+   
+    // recursively traverse the BST  
+    void inorder_Recursive(Node root) { 
+        if (root != null) { 
+            inorder_Recursive(root.left); 
+            System.out.print(root.key + " "); 
+            inorder_Recursive(root.right); 
+        } 
+    } 
+     
+    boolean search(int key)  { 
+        root = search_Recursive(root, key); 
+        if (root!= null)
+            return true;
+        else
+            return false;
+    } 
+   
+    //recursive insert function
+    Node search_Recursive(Node root, int key)  { 
+        // Base Cases: root is null or key is present at root 
+        if (root==null || root.key==key) 
+            return root; 
+        // val is greater than root's key 
+        if (root.key > key) 
+            return search_Recursive(root.left, key); 
+        // val is less than root's key 
+        return search_Recursive(root.right, key); 
+    } 
+}
+class Main{
+    public static void main(String[] args)  { 
+        //insert data into BST
+    	BST bst=new BST();
+        bst.insert(56); 
+        bst.insert(30); 
+        bst.insert(70); 
+        //print the BST
+        System.out.println("The BST Created with input data(Left-root-right):"); 
+        bst.inorder(); 
+     } 
 }
